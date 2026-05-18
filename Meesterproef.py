30f290a3987d915fb67be04316947047c25ae5ba
IMMXNEXTY <99086940@mydavinci.nl>
2026-05-18 11:24:21 +0200

    feature 14

--- Meesterproef.py
+++ Meesterproef.py
@@ -158,3 +158,103 @@ while True:
 
                 break
 
+
+    # ================= FEATURE 11 - BALLEN TREKKEN =================
+
+    if goed == True:
+
+        print("\nBallen trekken:")
+
+        for i in range(2):
+
+            bal = random.choice(ballenbak)
+
+            print("Bal:", bal)
+
+
+            # ================= FEATURE 12 - GROENE BAL =================
+
+            if bal == "groen":
+
+                if team == 1:
+                    team1_groen += 1
+
+                else:
+                    team2_groen += 1
+
+
+            # ================= FEATURE 13 - RODE BAL =================
+
+            elif bal == "rood":
+
+                if team == 1:
+                    team1_rood += 1
+
+                else:
+                    team2_rood += 1
+
+                print("Rode bal! Geen tweede trek.")
+
+                break
+
+
+            # ================= FEATURE 14 - NUMMERBAL =================
+
+            else:
+
+                for rij in bingokaart:
+
+                    for plek in range(len(rij)):
+
+                        if rij[plek] == bal:
+                            rij[plek] = "X"
+
+
+    # ================= FEATURE 15 - BINGOKAART TONEN =================
+
+    print("\nBINGOKAART")
+
+    for rij in bingokaart:
+        print(rij)
+
+
+    # ================= FEATURE 16 - BINGO CONTROLEREN =================
+
+    bingo = False
+
+
+    # horizontaal
+
+    for rij in bingokaart:
+
+        if rij == ["X", "X", "X", "X"]:
+            bingo = True
+
+
+    # verticaal
+
+    for i in range(4):
+
+        if (
+            bingokaart[0][i] == "X" and
+            bingokaart[1][i] == "X" and
+            bingokaart[2][i] == "X" and
+            bingokaart[3][i] == "X"
+        ):
+
+            bingo = True
+
+
+    # diagonaal
+
+    if (
+        bingokaart[0][0] == "X" and
+        bingokaart[1][1] == "X" and
+        bingokaart[2][2] == "X" and
+        bingokaart[3][3] == "X"
+    ):
+
+        bingo = True
+
+
+    
