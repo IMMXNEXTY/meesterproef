302be2d0a028051faf452f8eed38b1b0782b51ba
IMMXNEXTY <99086940@mydavinci.nl>
2026-05-18 10:59:22 +0200

    sorry vorige was feature 3 dit is 4
    s

--- Meesterproef.py
+++ Meesterproef.py
@@ -2,8 +2,6 @@ from woordenlijst import kies_woord
 import random
 
 
-
-
 team = 1
 
 team1_score = 0
@@ -19,12 +17,21 @@ team1_fout = 0
 team2_fout = 0
 
 
-
-
 ballenbak = [
     "groen", "groen", "groen",
     "rood", "rood", "rood",
     2, 4, 6, 8,
     1, 3, 5, 7
 ]
+
+
+
+
+bingokaart = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+]
+
 
